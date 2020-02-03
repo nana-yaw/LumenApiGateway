0a21d2c3953f9d6e935c341e41a09b1010ba5e8b
nana-yaw <edward.osei.nyarko@gmail.com>
2020-02-03 11:25:04 +0000

    Api Gateway components prepared to consume internal services

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Author;
+use App\Services\AuthorService;
 use App\Traits\ApiResponser;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -11,14 +11,21 @@ class AuthorController extends Controller
 {
 
     use ApiResponser;
+
+    /**
+     * The service to consume the authors microservice
+     * @var AuthorService
+     */
+    public $authorService;
+
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(AuthorService $authorService)
     {
-        //
+        $this->authorService = $authorService;
     }
 
     /**
